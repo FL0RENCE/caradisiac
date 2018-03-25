58395ccd79170d2e53faa18df649d805d7e7ec32
FL0RENCE <florence.lelaidier@devinci.fr>
2018-03-25 17:43:21 +0200

    Create populate.js
    only comments are from me, tried to anderstand how this code works

---
+++ moi/populate.js
@@ -0,0 +1,62 @@
+// importation de la librairie express, permettant de faire la connexion du port
+var express = require('express');
+
+//minimap des différentes routes de l'app
+var router = express.Router();
+
+//instance de l'api
+var carapi = require('node-car-api')
+
+//libraire pour utiliser elasticsearch
+var elastic = require('../elasticsearch');
+
+//fonction qui ne bloque pas le déroulement de l'app puisque asynchrone
+var gather = async function () {
+
+//la table elakstic est indexée
+  elastic.createIndex()
+
+  try {
+  
+  //on attend la promesse de l'obtention des brands. On devrait obtenir une liste des marques
+    brands = await carapi.getBrands()
+    
+    //liste des modèles
+    var j = []
+    
+    for(let i in brands) {
+    
+      console.log(brands[i])
+      
+      /liste des modèles de la marque
+      m = await carapi.getModels(brands[i])
+      
+      //on met chaque modèle dans chaque marque. dans la table elastic
+      elastic.put('brand', m)
+      
+      j.push(m)
+    }
+    
+    console.log(j)
+
+  }catch(ex) {
+  
+    console.log("err "+ex)
+    
+    return ex
+  }
+
+    return j
+
+}
+
+router.get('/', function (req, res, next) {
+
+//construction du json
+  gather().then((a) => {
+    res.json(a)
+  })
+
+});
+
+module.exports = router;
